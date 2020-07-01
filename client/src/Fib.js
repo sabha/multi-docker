15114bc067742cd6f82b07685d6c03b180154c2a
Sabhanayagam B <sabhanayagam.b@cognizant.com>
2020-06-30 19:31:13 -0500

    sabha | Fixing the setState undefined value.

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -15,13 +15,13 @@ class Fib extends Component {
 
   async fetchValues() {
     const values = await axios.get('/api/values/current');
-    this.setState({ values: values.data });
+    this.setState({ values: values.data ? values.data : {}});
   }
 
   async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
     this.setState({
-      seenIndexes: seenIndexes.data
+      seenIndexes: seenIndexes.data ? seenIndexes.data : []
     });
   }
 
